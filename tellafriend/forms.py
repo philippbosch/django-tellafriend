45ce0c72e2924cb5f69e7fe55a43e25e175130cf
Philipp Bosch <pb@philippbosch.de>
2010-08-06 23:06:03 +0200

    Some small bugfixes (I18N, text, …).

--- tellafriend/forms.py
+++ tellafriend/forms.py
@@ -10,8 +10,8 @@ except ImportError:
 
 
 class TellAFriendForm(forms.Form):
-    url = forms.URLField(label=_("Seite"), widget=forms.HiddenInput)
+    url = forms.URLField(label=_("URL"), widget=forms.HiddenInput)
     email_sender = forms.EmailField(max_length=100, label=_("Sender"))
     email_recipient = forms.EmailField(max_length=100, label=_("Recipient"))
-    message = forms.CharField(required=False, widget=forms.Textarea, label=_("Message (optional)"))
-    captcha = CaptchaField(help_text=_("Please enter the characters shown in the image in the field below."))
+    message = forms.CharField(required=False, widget=forms.Textarea, label=_("Message"))
+    captcha = CaptchaField(help_text=_("Please enter the characters shown in the image in the field next to it."))
