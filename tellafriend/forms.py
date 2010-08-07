c9f9769591b3f31b9ac8a3c7fdc0cbe62e45aaa0
Philipp Bosch <pb@philippbosch.de>
2010-08-07 14:36:39 +0200

    Label for captcha field added.

--- tellafriend/forms.py
+++ tellafriend/forms.py
@@ -14,4 +14,4 @@ class TellAFriendForm(forms.Form):
     email_sender = forms.EmailField(max_length=100, label=_("Sender"))
     email_recipient = forms.EmailField(max_length=100, label=_("Recipient"))
     message = forms.CharField(required=False, widget=forms.Textarea, label=_("Message"))
-    captcha = CaptchaField(help_text=_("Please enter the characters shown in the image in the field next to it."))
+    captcha = CaptchaField(help_text=_("Please enter the characters shown in the image in the field next to it."), label=_("Captcha"))
